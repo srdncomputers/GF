8120843724717b72151f6cc1408366999e760a40
Rupesh Sinha <rupesh.sinha@srdncomputers.com>
2026-02-25 18:11:51 +0530

    f

--- Shilpiapp/ap.py
+++ Shilpiapp/ap.py
@@ -25,7 +25,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # -----------------------------------
-# FUNCTIONSs
+# FUNCTIONS
 # -----------------------------------
 def build_persona(grade, subject):
     return f"""
