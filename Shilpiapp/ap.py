72f3c7a529b9af0df7e75bc0da2b8e961092382a
Rupesh Sinha <rupesh.sinha@srdncomputers.com>
2026-03-02 10:43:17 +0530

    d

--- Shilpiapp/ap.py
+++ Shilpiapp/ap.py
@@ -167,17 +167,18 @@ section[data-testid="stSidebar"] label {
     color: white !important;
 }
 
-/* ===== FIX DROPDOWN TEXT TO BLACK ===== */
-
+/* ===== Sidebar Selectbox Text Black ===== */
 section[data-testid="stSidebar"] select {
     color: black !important;
     background-color: #f1f5f9 !important;
 }
 
+/* Selected value inside custom select */
 section[data-testid="stSidebar"] div[data-baseweb="select"] span {
     color: black !important;
 }
 
+/* Dropdown menu options */
 div[role="listbox"] div {
     color: black !important;
 }
